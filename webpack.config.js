73c1b6f6ec54347d7afefe6cb0e5037c35907302
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-27 12:39:25 +0000

    initial setup for webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: ['./src/main'],
+    output: {
+        path: require('path').join(__dirname, 'app'),
+        filename: 'dist/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
